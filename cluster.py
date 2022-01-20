7aae194b13959ef04e3520ce7c851487463ff7fa
paulwababu <paulkiragu621@gmail.com>
2022-01-20 12:16:23 +0300

    added django file and readme

--- cluster.py
+++ cluster.py
@@ -1,1 +1,21 @@
 from math import sqrt
+
+"""
+To calculate kcse cluster points, we use the formular below
+
+C = √(X/48 * Y/84) * 48
+
+where, C = Cluster Points
+       X = points obtained in the four main subjects required in the course you want to do,
+       eg computer science
+       Y = total points of the seven subjects
+       
+       ***EXAMPLE**
+       1. Nursing Course Cluster Calculation
+        - Nursing course needs Biology, Chemistry, Maths and English as Compulsory subjects
+          The subjects above when converted to points , eg A for 12, adds up to X in the equation above
+          Lets say you got: Biology C+(7) History C+(7) Maths B-(8) English B+(10)  
+          Your total X will be 
+        -   
+
+"""
